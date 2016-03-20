885e8c4dbdf08bb839ff5f0ac82d9538a6269801
44797729@mylife.unisa.ac.za <44797729@mylife.unisa.ac.za>
2016-03-20 16:18:21 +0200

    Added an Empty Database and Added CRUD Skeleton Functions and added the tables in the databases with example data

--- Datalayer/Datalayer.vb
+++ Datalayer/Datalayer.vb
@@ -1,7 +1,10 @@
 ﻿Imports System.Data.OleDb
 Imports System.IO
 Imports Microsoft.VisualBasic.FileIO
-Imports System.Reflection
+Imports System.Reflection 
+Imports System.Data.SqlClient
+Imports System.Configuration
+
 
 Public Class Datalayer
 
@@ -54,8 +57,6 @@ Public Class Datalayer
         Return dssample
     End Function
 
-
-
     Public Function ConvertCsvToDatatableExtended(filename As String) As DataTable
         '   Dim fileName = "C:\unisa\ICT3611\ASSIGNMENT 1\Assignment1\Datalayer\Flatfiles\Customer.csv"
         Dim delimiters As String = ","
@@ -91,4 +92,121 @@ Public Class Datalayer
 
         Return result
     End Function
+
+    'In this Assignment we are going to use MS Access to store Data. 
+#Region "Assignment 2"
+
+
+#Region "Connection objects"
+    ReadOnly strCon As [String] = ConfigurationManager.ConnectionStrings("GoAml").ConnectionString
+    Private cmd As SqlCommand
+    Private con As SqlConnection
+    Private da As SqlDataAdapter
+#End Region
+
+#Region "Athletes"
+    Public Function Report_AC_DESC(AC_DESC As String) As DataTable
+        con = New SqlConnection(strCon)
+        cmd = New SqlCommand()
+        cmd.CommandType = System.Data.CommandType.StoredProcedure
+        cmd.CommandTimeout = 0
+        cmd.Connection = con
+        cmd.CommandText = "[motovate].[spx_Search_AC_DESC]"
+        cmd.Parameters.Add("@AC_DESC", SqlDbType.VarChar).Value = AC_DESC
+
+        Dim dt As New DataTable()
+        da = New SqlDataAdapter()
+        da.SelectCommand = cmd
+
+
+        Try
+            con.Open()
+            da.Fill(dt)
+        Catch ex As SqlException
+            Throw ex
+        Finally
+            con.Close()
+        End Try
+        Return dt
+
+    End Function
+    Public Function CheckUserExistance(_sUSERNAME As [String]) As Integer
+        Me.con = New SqlConnection(Me.strCon)
+
+        Me.cmd = New SqlCommand()
+
+        Me.cmd.CommandText = "sp_CheckUserExistance"
+
+        Me.cmd.CommandType = CommandType.StoredProcedure
+
+        Me.cmd.Connection = Me.con
+
+        Me.cmd.Parameters.Add("@USERNAME", SqlDbType.VarChar).Value = _sUSERNAME
+
+        Me.cmd.Parameters.Add("@RESULTS", SqlDbType.Int)
+
+        Me.cmd.Parameters("@RESULTS").Direction = ParameterDirection.Output
+
+        Dim isValid As Integer = 0
+
+        Try
+            Me.con.Open()
+
+            Me.cmd.ExecuteNonQuery()
+
+            isValid = CInt(Me.cmd.Parameters("@RESULTS").Value)
+        Catch ex As SqlException
+            Throw ex
+        Finally
+            Me.con.Close()
+        End Try
+        Return isValid
+    End Function
+    Public Function AuthonticateUser(sUsername As [String], sPassword As [String]) As Integer
+        Me.con = New SqlConnection(Me.strCon)
+
+        Me.cmd = New SqlCommand()
+
+        Me.cmd.CommandText = "sp_Authonticateuser"
+
+        Me.cmd.CommandType = CommandType.StoredProcedure
+
+        Me.cmd.Connection = Me.con
+
+        Me.cmd.Parameters.Add("@USERNAME", SqlDbType.VarChar).Value = sUsername
+
+        Me.cmd.Parameters.Add("@PASSWORD", SqlDbType.VarChar).Value = sPassword
+
+        Me.cmd.Parameters.Add("@RESULTS", SqlDbType.Int)
+
+        Me.cmd.Parameters("@RESULTS").Direction = ParameterDirection.Output
+
+        Dim isValid As Integer = 0
+
+
+        Try
+            Me.con.Open()
+
+            Me.cmd.ExecuteNonQuery()
+
+        Catch ex As SqlException
+            Throw ex
+        Finally
+            Me.con.Close()
+        End Try
+        Return isValid
+    End Function
+
+#End Region
+
+#Region "Events"
+
+#End Region
+
+
+
+ 
+
+#End Region
+
 End Class
