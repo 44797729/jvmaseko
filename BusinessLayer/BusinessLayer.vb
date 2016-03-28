1112d50d5e49b409a3d45457d465318602086744
44797729@mylife.unisa.ac.za <44797729@mylife.unisa.ac.za>
2016-03-28 10:32:17 +0200

    Add Events Bussisness Functions

--- BusinessLayer/BusinessLayer.vb
+++ BusinessLayer/BusinessLayer.vb
@@ -51,8 +51,8 @@ Public Class BusinessLayer
         End Try
     End Function
 
-
-
+#Region "Oledb Athlete"
+     
     Public Sub UpdateAthelete(model As AtheleteModel)
 
         Dim obj As New Datalayer.Datalayer()
@@ -79,8 +79,7 @@ Public Class BusinessLayer
         Catch ex As Exception
 
         End Try
-    End Function
-
+    End Function 
     Public Sub DeleteAthlete(model As AtheleteModel)
 
         Dim obj As New Datalayer.Datalayer()
@@ -92,4 +91,50 @@ Public Class BusinessLayer
 
 
     End Sub
+
+#End Region
+
+
+#Region "Oledb Events"
+
+    Public Sub UpdateEvents(model As AtheleteModel)
+
+        Dim obj As New Datalayer.Datalayer()
+        Try
+            obj.UpdateEvents(model)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Public Sub AddEvents(model As AtheleteModel)
+
+        Dim obj As New Datalayer.Datalayer()
+        Try
+            obj.AddEvents(model)
+        Catch ex As Exception
+
+        End Try
+    End Sub
+    Public Function GetEvents() As DataTable
+
+        Dim obj As New Datalayer.Datalayer()
+        Try
+            Return obj.GetEvents()
+        Catch ex As Exception
+
+        End Try
+    End Function
+    Public Sub DeleteEvents(model As AtheleteModel)
+
+        Dim obj As New Datalayer.Datalayer()
+        Try
+            obj.DeleteEvents(model)
+        Catch ex As Exception
+
+        End Try
+
+
+    End Sub
+
+#End Region
 End Class
