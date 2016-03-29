ec10a9499a218489fa32fa09619f78cfdb1e86f7
44797729@mylife.unisa.ac.za <44797729@mylife.unisa.ac.za>
2016-03-29 21:28:19 +0200

    Cleaned the code all the Code is working

--- BusinessLayer/BusinessLayer.vb
+++ BusinessLayer/BusinessLayer.vb
@@ -1,55 +1,8 @@
 ﻿Imports Datalayer
 
 Public Class BusinessLayer
-
-
-    Public Sub Convert_DataTable_To_CSV(ByVal dtable As DataTable, ByVal pathFilename As String, ByVal sepChar As String)
-
-        Dim obj As New Datalayer.Datalayer()
-        Try
-            obj.Convert_DataTable_To_CSV(dtable, pathFilename, sepChar)
-
-        Catch ex As Exception
-
-        End Try
-
-
-    End Sub
-
-
-    Public Function ConvertCsvToDatatable(ByVal dtable As DataTable, ByVal pathFilename As String, ByVal sepChar As String)
-
-        Dim obj As New Datalayer.Datalayer()
-        Try
-            Return obj.ConvertCsvToDatatable(dtable, pathFilename, sepChar)
-
-        Catch ex As Exception
-
-        End Try
-    End Function
-
-    Public Function Getdata()
-        Dim obj As New Datalayer.Datalayer()
-        Try
-            Return obj.Getdata()
-
-        Catch ex As Exception
-
-        End Try
-    End Function
-
-
-
-    Public Function ConvertCsvToDatatableExtended(Filename As String) As DataTable
-        Dim obj As New Datalayer.Datalayer()
-        Try
-            Return obj.ConvertCsvToDatatableExtended(Filename)
-
-        Catch ex As Exception
-        Finally
-            obj = Nothing
-        End Try
-    End Function
+   
+ 
 
 #Region "Oledb Athlete"
      
