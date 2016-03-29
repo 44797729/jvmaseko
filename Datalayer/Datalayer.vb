9a813872cf3f273ec0bcdb06f82c91f74abeee6a
44797729@mylife.unisa.ac.za <44797729@mylife.unisa.ac.za>
2016-03-29 21:29:12 +0200

    Now the Data layer has a complete

--- Datalayer/Datalayer.vb
+++ Datalayer/Datalayer.vb
@@ -8,92 +8,6 @@ Imports System.Configuration
 
 Public Class Datalayer
 
-    Public Sub Convert_DataTable_To_CSV(ByVal dtable As DataTable, ByVal pathFilename As String, ByVal sepChar As String)
-        Dim writer As System.IO.StreamWriter
-        Try
-            writer = New System.IO.StreamWriter(pathFilename)
-
-            Dim _sep As String = ""
-            Dim builder As New System.Text.StringBuilder
-            For Each col As DataColumn In dtable.Columns
-                builder.Append(_sep).Append(col.ColumnName)
-                _sep = sepChar
-            Next
-            writer.WriteLine(builder.ToString())
-
-            For Each row As DataRow In dtable.Rows
-                _sep = ""
-                builder = New System.Text.StringBuilder
-
-                For Each col As DataColumn In dtable.Columns
-                    builder.Append(_sep).Append(row(col.ColumnName))
-                    _sep = sepChar
-                Next
-                writer.WriteLine(builder.ToString())
-            Next
-        Catch ex As Exception
-
-        Finally
-            If Not writer Is Nothing Then writer.Close()
-        End Try
-    End Sub
-
-    Public Function Getdata()
-        Return ConvertCsvToDatatable()
-    End Function
-
-
-    Public Function ConvertCsvToDatatable()
-        Dim folder As String = Path.Combine(Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location), "Flatfiles\")
-
-        'Dim folder = "C:\unisa\ICT3611\ASSIGNMENT 1\Assignment1\Datalayer\Flatfiles\"
-        Dim cnStr = "Provider=Microsoft.Jet.OLEDB.4.0;Data Source=" & folder & ";Extended Properties=""text;HDR=No;FMT=Delimited"";"
-
-        Dim dssample As New DataTable
-
-        Using adp As New SqlDataAdapter("select * from [Customer.csv]", cnStr)
-            adp.Fill(dssample)
-        End Using
-        Return dssample
-    End Function
-
-    Public Function ConvertCsvToDatatableExtended(filename As String) As DataTable
-        '   Dim fileName = "C:\unisa\ICT3611\ASSIGNMENT 1\Assignment1\Datalayer\Flatfiles\Customer.csv"
-        Dim delimiters As String = ","
-        Dim firstRowContainsFieldNames As Boolean = True
-        Dim result As New DataTable()
-
-        Using tfp As New TextFieldParser(filename)
-            tfp.SetDelimiters(delimiters)
-
-            ' Get Some Column Names
-            If Not tfp.EndOfData Then
-                Dim fields As String() = tfp.ReadFields()
-
-                For i As Integer = 0 To fields.Count() - 1
-                    If firstRowContainsFieldNames Then
-                        result.Columns.Add(fields(i))
-                    Else
-                        result.Columns.Add("Col" + i)
-                    End If
-                Next
-
-                ' If first line is data then add it
-                If Not firstRowContainsFieldNames Then
-                    result.Rows.Add(fields)
-                End If
-            End If
-
-            ' Get Remaining Rows
-            While Not tfp.EndOfData
-                result.Rows.Add(tfp.ReadFields())
-            End While
-        End Using
-
-        Return result
-    End Function
-
-    'In this Assignment we are going to use MS Access to store Data. 
 #Region "Assignment 2"
 
 
@@ -397,12 +311,11 @@ Public Class Datalayer
         Me.cmdole.Connection = Me.conole
 
         Me.cmdole.Parameters.Add("@Eventid", SqlDbType.VarChar).Value = model.Eventid.ToString.Replace("{", "").Replace("}", "").ToUpper()
-         
-        Me.cmdole.Parameters.Add("@EventDate", SqlDbType.VarChar).Value = model.EventDate.ToString()
-         
+        Me.cmdole.Parameters.Add("@Membershipnumber", SqlDbType.VarChar).Value = model.Membershipnumber
+        Me.cmdole.Parameters.Add("@EventDate", SqlDbType.VarChar).Value = model.EventDate.ToString() 
         Me.cmdole.Parameters.Add("@EventResults", SqlDbType.VarChar).Value = model.EventResults
 
-        Me.cmdole.Parameters.Add("@Membershipnumber", SqlDbType.VarChar).Value = model.Membershipnumber
+
 
         Try
             Me.conole.Open()
@@ -423,14 +336,12 @@ Public Class Datalayer
 
         Me.cmdole.CommandType = CommandType.Text 
         Me.cmdole.Connection = Me.conole
-
-        Me.cmdole.Parameters.Add("@Athleteid", SqlDbType.VarChar).Value = model.Eventid.ToString.Replace("{", "").Replace("}", "").ToUpper()
-
-        Me.cmdole.Parameters.Add("@EventDate", SqlDbType.VarChar).Value = model.EventDate
-
+  
+        Me.cmdole.Parameters.Add("@Eventid", SqlDbType.VarChar).Value = model.Eventid.ToString.Replace("{", "").Replace("}", "").ToUpper()
+        Me.cmdole.Parameters.Add("@Membershipnumber", SqlDbType.VarChar).Value = model.Membershipnumber
+        Me.cmdole.Parameters.Add("@EventDate", SqlDbType.VarChar).Value = model.EventDate.ToString()
         Me.cmdole.Parameters.Add("@EventResults", SqlDbType.VarChar).Value = model.EventResults
 
-        Me.cmdole.Parameters.Add("@membershipnumber", SqlDbType.VarChar).Value = model.Membershipnumber
          
         Try
             Me.conole.Open()
@@ -448,7 +359,8 @@ Public Class Datalayer
 
         Me.cmdole = New OleDbCommand()
 
-        Me.cmdole.CommandText = "DELETE from AtheleteEvents WHERE Athleteid='" & model.Eventid.ToString.Replace("{", "").Replace("}", "").ToUpper() & "' And Membershipnumber=" & model.Membershipnumber
+        Me.cmdole.CommandText = "DELETE from AtheleteEvents WHERE Eventid='" & model.Eventid.ToString.Replace("{", "").Replace("}", "").ToUpper() & "' And Membershipnumber='" & model.Membershipnumber & "'"
+
 
         Me.cmdole.CommandType = CommandType.Text
 
